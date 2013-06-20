dc863e167b7b0b26509a22e737aa9113bf9b9770
André Gonçalves <andreslb1@gmail.com>
2013-06-20 16:47:04 +0100

    maps zoom

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -27,7 +27,7 @@ define(["jquery", "backbone", "cordova", "gmap", "async!http://maps.google.com/m
                     $('#location_map').gmap('addMarker', {
                         'position': clientPosition,
                         'bounds': true,
-                        'zoom': 7
+                        'zoom': 5
                     });
                     $.mobile.loading("hide");
                 }, function(error) {
@@ -81,7 +81,7 @@ define(["jquery", "backbone", "cordova", "gmap", "async!http://maps.google.com/m
                 $('#location_map').gmap('addMarker', {
                     'position': '40.208696,-8.425400',
                     'bounds': true,
-                    'zoom': 7
+                    'zoom': 5
                 });
             });
 
