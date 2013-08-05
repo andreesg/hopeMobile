0ca8d6b49de52015255a6b192f487eca40d1f6d3
André Gonçalves <andreslb1@gmail.com>
2013-08-05 19:17:51 +0100

    gallery working

--- js/views/DetailsView.js
+++ js/views/DetailsView.js
@@ -16,9 +16,9 @@ define(["jquery", "backbone", "photoswipe", "../collections/PhotoCollection"], f
         initialize: function() {
             _.bindAll(this, "goBack", "render", "addPhoto", "vote");
             var that = this;
-
+            console.log(that.model.id);
             this.photoList = new PhotoCollection(null, {
-                occurr_id: that.model.get('id') 
+                occurr_id: that.model.id 
             });
 
             this.render();
@@ -56,8 +56,18 @@ define(["jquery", "backbone", "photoswipe", "../collections/PhotoCollection"], f
 
         renderPhotos: function(collection, response) {
             console.log("[DetailsView] Render Photos.");
-            console.log(collection);
-            var photoSwipeInstance = $("#gallery1 a").photoSwipe({enableMouseWheel:false, enableKeyboard:false});
+            //console.log(collection);
+            
+            var staticUrl = "http://webmoth.dec.uc.pt/static/";
+            var template = "";
+            collection.each(function(model) {
+                template += "<li><a href='"+staticUrl+model.get("path_big")+"' rel='external'><img src='"+staticUrl+model.get("path_small")+"'/></a></li>";
+            });
+
+            if (collection.length > 0) {
+                $("#gallery1").html(template);
+                var photoSwipeInstance = $("#gallery1 a").photoSwipe({enableMouseWheel:false, enableKeyboard:false});
+            }
         },
 
         render: function() {
